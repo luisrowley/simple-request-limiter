49df8d8d188ed35874bc57497433382c1479d143
luisrowley <louisdebenito@gmail.com>
2023-10-09 12:02:59 +0200

    changed default token amount to burst.

---
+++ rateLimitService.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const config = require('./config.json');
+
+app.use(bodyParser.json());
+
+const TIME_MS = 60 * 1000;
+const tokenBuckets = new Map();
+// bucket initialization
+config.rateLimitsPerEndpoint.forEach(endpoint => {
+    tokenBuckets.set(endpoint.endpoint, {
+       tokens: endpoint.burst,
+       burst: endpoint.burst,
+       sustained: endpoint.sustained, 
+       lastRefresh: Date.now()
+    });
+});
+
+function calculateTokensToAdd(elapsedTime, sustainedRate) {
+    return Math.floor((elapsedTime / TIME_MS) * sustainedRate);
+}
+
+// rate-limit middleware
+app.post('/take', (req, res) => {
+    const { routeTemplate } = req.body;
+    
+    if (!routeTemplate) {
+        return res.status(400).json({
+            error: 'Route template is required.' 
+        });
+    }
+    
+    if (tokenBuckets.has(routeTemplate)) {
+        const bucket = tokenBuckets.get(routeTemplate);
+        const currentTime = Date.now();
+        const elapsedTime = currentTime - bucket.lastRefresh;
+        const sustainedRate = bucket.sustained;
+        
+        const tokensToAdd = calculateTokensToAdd(elapsedTime, sustainedRate);
+        bucket.tokens = Math.min(bucket.tokens + tokensToAdd, bucket.burst);
+        bucket.lastRefresh = currentTime;
+        
+        if (bucket.tokens > 0) {
+            bucket.tokens--;
+            return res.status(200).json({
+                remainingTokens: bucket.tokens,
+                allowed: true
+            });
+        } else {
+            return res.status(200).json({
+                remainingTokens: 0,
+                allowed: false
+            });
+        }
+    } else {
+        return res.status(404).json({
+            error: 'Route not found.'
+        });
+    }
+});
+
+module.exports = app;
---
+++ test/rate-limit.test.js
@@ -0,0 +1,59 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../rateLimitService');
+
+const { expect } = chai;
+chai.use(chaiHttp);
+
+describe('Rate Limit Service', () => {
+    it('should respond with remaining tokens and allowed status', (done) => {
+        const routeTemplate = 'GET /user/:id';
+        chai.request(app)
+            .post('/take')
+            .send({ routeTemplate })
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.body).to.have.property('remainingTokens').to.equal(9); 
+                expect(res.body).to.have.property('allowed');
+                done();
+            });
+    });
+    
+    it('should reject requests after rate limit is exceeded', (done) => {
+        const routeTemplate = 'GET /user/:id';
+        const requests = Array.from({ length: 55 }, (_, index) => index + 1);
+        const promises = requests.map(() => {
+            return chai.request(app)
+                .post('/take')
+                .send({ routeTemplate });
+        });
+
+        // remaining tokens: 9
+        Promise.all(promises)
+            .then((responses) => {
+                for (let i = 0; i < 9; i++) {
+                    expect(responses[i]).to.have.status(200);
+                    expect(responses[i].body).to.have.property('remainingTokens').to.be.a('number');
+                    expect(responses[i].body).to.have.property('allowed').to.be.true;
+                }
+                expect(responses[10]).to.have.status(200);
+                expect(responses[10].body).to.have.property('remainingTokens').to.equal(0);
+                expect(responses[10].body).to.have.property('allowed').to.be.false;
+                // remaining tokens: 0
+                done();
+            })
+            .catch((error) => done(error));
+    });
+
+
+    it('should handle missing route template and respond with 400', (done) => {
+        chai.request(app)
+            .post('/take')
+            .end((err, res) => {
+                expect(res).to.have.status(400);
+                expect(res.body).to.be.an('object');
+                expect(res.body).to.have.property('error');
+                done();
+            });
+    });
+});
